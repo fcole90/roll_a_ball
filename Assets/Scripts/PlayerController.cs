bd40e33fdd3ed05604892acd7afbf1bc96f081d5
Fabio Colella <fcole90@gmail.com>
2019-12-07 14:21:25 +0200

    moving the player (2 of 8): Done!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // FixedUpdate is called once per frame before the Update()
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
+        rb.AddForce(movement);
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Roll a ball
+
+Doing Unity's introductory tutorial.
+
+[Video tutorial](https://www.youtube.com/watch?v=RFlh8pTf4DU&list=PLX2vGYjWbI0Q-s4_lX0h4i2zbZqlg4OfF)
